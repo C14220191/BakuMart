a37b20a450b08e2ca7017c3b515374e8cbc8bbe8
JasonAS <153482291+C14220191@users.noreply.github.com>
2025-06-06 07:30:35 +0700

    a

--- routes/web.php
+++ routes/web.php
@@ -11,4 +11,7 @@ Route::resource('home', ProductController::class);
 Route::get('/login', function () {
     return view('login');
 })->name('login');
+Route::get('/register', function () {
+    return view('register');
+})->name('register');
 
